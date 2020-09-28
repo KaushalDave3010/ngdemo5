7accc25e994158c80df1267b77bc97c391ecfc7e
Kaushal Dave <kndave3010@gmail.com>
2020-09-28 15:46:35 +0530

    first commit

---
+++ Practise/ForGitHub/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ArrayExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Enter the size of array :- ");
+
+            n = int.Parse(Console.ReadLine());
+
+            int[] a = new int[n];
+            
+            Console.Write("Enter The Array :- ");
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Reverse(a);
+
+            Console.WriteLine("Reverced Array :- ");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}",a[i]);
+            }
+
+            Console.WriteLine("First Element of array is {0}",a[0]);
+            Console.WriteLine("Last Element of array is {0}",a[n-1]);
+
+            
+        }
+    }
+}
